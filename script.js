9c4ec39e1faab52d5a3bf34c7ac5574c4f42c32c
Bona CN <bonachidera65@gmail.com>
2025-05-05 00:45:12 +0100

    cleaned script and uncommented final animations

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ let circle = document.querySelector(".circle");
     yoyo: true
   });
 
-gsap.to(box, {
-    y: "40vh",       // move UP 50% of the viewport height
-    duration: 0.8,     // quicker to simulate bounce speed
-    ease: "bounce.out",
-    repeat: -1,        // infinite bouncing
-    yoyo: true,        // bounce back to the ground
-    delay: 0.5         // small delay before first bounce
-  });
+// gsap.to(box, {
+//     y: "40vh",       // move UP 50% of the viewport height
+//     duration: 0.8,     // quicker to simulate bounce speed
+//     ease: "bounce.out",
+//     repeat: -1,        // infinite bouncing
+//     yoyo: true,        // bounce back to the ground
+//     delay: 0.5         // small delay before first bounce
+//   });
